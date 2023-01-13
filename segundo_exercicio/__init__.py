105b22917cffae1d63016ee0e15a1f4c6f070880
Victor Nogueira <victorjose.nogueirasantos@hotmail.com>
2023-01-13 16:45:20 -0300

    1.0.1

--- segundo_exercicio/__init__.py
+++ segundo_exercicio/__init__.py
@@ -1,6 +1,6 @@
 # Passo 1: Importar a base de dados
 import pandas as pd
-
+import plotly.express as px
 tabela = pd.read_csv(r"C:\Users\T-GAMER\Desktop\aula intensivão\Aula 2\telecom_users.csv")
 
 # Passo 2: Visualizar a base de dados
@@ -24,3 +24,35 @@ print(tabela.info())
 # Como estão os nossos cancelamentos?
 print(tabela["Churn"].value_counts())
 print(tabela["Churn"].value_counts(normalize=True).map("{:.1%}".format))
+
+#1º etapa criar grafico
+#criar um grafico para cada coluna
+#for coluna in tabela.columns:
+ #   grafico = px.histogram(tabela, x=coluna, color='Churn', text_auto=True)
+
+    #2º etapa mostrar grafico
+  #  grafico.show()
+
+resumo = '''
+- clientes com mais familiares tendem a ter menos cancelamento
+    - promoções diferenciadas para mais pessoas da mesma familia
+- clientes nos primeiros meses tem uma tendencia muito MUITO a cacelarem
+    - pode ser algum marketing muito agressivo
+    - experiencia nos primeiros meses ruins
+    -planos mais baratos nos primeiros anos
+- clientes com mais serviços cancelam menos
+    - clientes com serviço de fibra cancelam mais do que os que nao tem
+    - clientes sem serviço de segurança online tendem a cancelare mais
+    - clientes sem serviço de backup online cancelam mais 
+    - clientes sem serviço de proteção ao equipamentos tendem a cancelarem mais
+    - clientes sem suporte tecnico cancelam mais 
+    
+        * podemos oferecer maior quantidade de produtos com preço reduzido ou zerado
+        
+- Quase todos cancelamentos estao no contrato mensal
+    * oferecer descontos nos outros planos
+
+- clientes que pagam no boleto tendem a cancelar mais
+'''
+
+print(resumo)
