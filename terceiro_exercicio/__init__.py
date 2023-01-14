b29ffe3be82eef97fb3dec5b5108c0e6b2ac3a93
Victor Nogueira <victorjose.nogueirasantos@hotmail.com>
2023-01-14 02:02:41 -0300

    1.0.5

--- terceiro_exercicio/__init__.py
+++ terceiro_exercicio/__init__.py
@@ -9,8 +9,7 @@ from selenium.webdriver.common.keys import Keys
 
 
 
-print(f'{float(cotacao_euro):.2f}')
-print(f'{float(cotacao_dolar):.2f}')
+
 
 #1: conferir cotações
     #1.1: cotação do dolar
@@ -21,18 +20,15 @@ navegador.find_element('xpath', '/html/body/div[1]/div[3]/form/div[1]/div[1]/div
 cotacao_dolar = navegador.find_element('xpath', '//*[@id="knowledge-currency__updatable-data-column"]/div[1]/div[2]/span[1]').get_attribute('data-value')
 
     #1.2: cotação do euro
-navegador.find_element('xpath', '//*[@id="tsf"]/div[1]/div[1]/div[2]/div/div[2]/input').click()
-navegador.find_element('xpath', '//*[@id="tsf"]/div[1]/div[1]/div[2]/div/div[2]/input').clear()
-navegador.find_element('xpath', '//*[@id="tsf"]/div[1]/div[1]/div[2]/div/div[2]/input').send_keys('cotação euro')
-navegador.find_element('xpath', '//*[@id="tsf"]/div[1]/div[1]/div[2]/div/div[2]/input').send_keys(Keys.ENTER)
+navegador = webdriver.Chrome()
+navegador.get(r'https://www.google.com/')
+navegador.find_element('xpath', '/html/body/div[1]/div[3]/form/div[1]/div[1]/div[1]/div/div[2]/input').send_keys('cotação euro')
+navegador.find_element('xpath', '/html/body/div[1]/div[3]/form/div[1]/div[1]/div[1]/div/div[2]/input').send_keys(Keys.ENTER)
 cotacao_euro = navegador.find_element('xpath', '//*[@id="knowledge-currency__updatable-data-column"]/div[1]/div[2]/span[1]').get_attribute('data-value')
-cotacao_euro
-    #1.3: cotação do ouro
-navegador.find_element('xpath', '//*[@id="tsf"]/div[1]/div[1]/div[2]/div/div[2]/input').click()
-navegador.find_element('xpath', '//*[@id="tsf"]/div[1]/div[1]/div[2]/div/div[2]/input').clear()
-navegador.find_element('xpath', '//*[@id="tsf"]/div[1]/div[1]/div[2]/div/div[2]/input').send_keys('cotação ouro')
-navegador.find_element('xpath', '//*[@id="tsf"]/div[1]/div[1]/div[2]/div/div[2]/input').send_keys(Keys.ENTER)
-cotacao_ouro = navegador.find_element('xpath', )
+   #1.3: cotação do ouro
+
+print(f'{float(cotacao_euro):.2f}')
+print(f'{float(cotacao_dolar):.2f}')
 #2: exportar base de dados
 #3: alterar valores da base de dados
 #4: exportar base de dados
