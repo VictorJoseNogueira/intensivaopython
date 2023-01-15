5e91d8eda2a8c416cd3dc9d66c50828cdd188a24
Victor Nogueira <victorjose.nogueirasantos@hotmail.com>
2023-01-15 16:19:25 -0300

    1.0.0

---
+++ quarto_exercicio/__init__.py
@@ -0,0 +1,16 @@
+import pandas as pd
+
+# passo 1: entender o desafio
+# passo 2: entendimento da area/empresa
+# passo 3: obtenção de dados
+tabela = pd.read_csv(r"C:\Users\T-GAMER\Desktop\aula intensivão\Aula 4\advertising.csv")
+print(tabela)
+print(tabela.corr())
+print(tabela.info())
+# passo 4: tratamento de dados
+#nesse exercicio essa etapa ja foi feita pelo lira
+# passo 5: analise exploratoria
+
+
+# passo 6: modelagem + algoritimo(IA se necessario )
+# passo 7: interpretação de resultados
