caa657ef19e17dd5c73104e001564c400e98ac54
Victor Nogueira <victorjose.nogueirasantos@hotmail.com>
2023-01-15 14:18:13 -0300

    1.1

--- terceiro_exercicio/__init__.py
+++ terceiro_exercicio/__init__.py
@@ -1,8 +1,9 @@
-from time import sleep
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
-
-
+import pandas as pd
+import datetime as dt
+x = dt.date.today()
+print(x)
 
 
 
@@ -26,9 +27,42 @@ navegador.find_element('xpath', '/html/body/div[1]/div[3]/form/div[1]/div[1]/div
 navegador.find_element('xpath', '/html/body/div[1]/div[3]/form/div[1]/div[1]/div[1]/div/div[2]/input').send_keys(Keys.ENTER)
 cotacao_euro = navegador.find_element('xpath', '//*[@id="knowledge-currency__updatable-data-column"]/div[1]/div[2]/span[1]').get_attribute('data-value')
    #1.3: cotação do ouro
+navegador = webdriver.Chrome()
+navegador.get(r'https://www.google.com/')
+navegador.find_element('xpath', '/html/body/div[1]/div[3]/form/div[1]/div[1]/div[1]/div/div[2]/input').send_keys('https://www.melhorcambio.com/ouro-hoje')
+navegador.find_element('xpath', '/html/body/div[1]/div[3]/form/div[1]/div[1]/div[1]/div/div[2]/input').send_keys(Keys.ENTER)
+navegador.find_element('xpath', '//*[@id="rso"]/div[1]/div/div/div/div[1]/div/div/div[1]/div/a/h3').click()
+cotacao_ouro = navegador.find_element('xpath', '//*[@id="comercial"]').get_attribute('value')
+cotacao_ouro = cotacao_ouro.replace(",", ".")
+cotacao_ouro = float(cotacao_ouro)
+cotacao_euro = float(cotacao_euro)
+cotacao_dolar = float(cotacao_dolar)
 
-print(f'{float(cotacao_euro):.2f}')
-print(f'{float(cotacao_dolar):.2f}')
+
+navegador.quit()
+
+
+
 #2: exportar base de dados
+tabela = pd.read_excel('Produtos.xlsx')
+
+
 #3: alterar valores da base de dados
+tabela.loc[tabela['Moeda'] == 'Dólar', 'Cotação'] = float(cotacao_dolar)
+tabela.loc[tabela['Moeda'] == 'Euro', 'Cotação'] = float(cotacao_euro)
+tabela.loc[tabela['Moeda'] == 'Ouro', 'Cotação'] = float(cotacao_ouro)
+#preço de compra = preço original X cotação
+tabela['Preço de Compra'] = tabela['Preço Original'] * tabela['Cotação']
+
+#preço de venda = preço de compra X margem
+tabela['Preço de Venda'] = tabela['Preço de Compra'] * tabela['Margem']
+
+#formatando
+tabela['Preço de Compra'] = tabela['Preço de Compra'].map("RS{:.2f}".format)
+tabela['Preço de Venda'] = tabela['Preço de Venda'].map("RS{:.2f}".format)
+tabela['Cotação'] = tabela['Cotação'].map("R${:.2f}".format)
+print(x)
+
+
 #4: exportar base de dados
+tabela.to_excel(f'produtos{x}.xlsx', index=False)
